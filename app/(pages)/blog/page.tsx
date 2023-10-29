789fac5185ea5c1a5eb9bf54aa0a7318e71d6975
AntonioGally <antonio.gally@gmail.com>
2023-10-29 18:43:42 -0300

    Adding new pages file

---
+++ app/(pages)/blog/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return (
+        <h1>Página do Blog</h1>
+    )
+}
+
+export default Page;
---
+++ app/(pages)/contact/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return (
+        <h1>Página do contato</h1>
+    )
+}
+
+export default Page;
